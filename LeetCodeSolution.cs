47a1602e398b6947e262db92422afd13f7f849bd
Imran Chaudhry <ichaudhry@gmail.com>
2022-12-22 09:32:39 +0100

    Working solution with some refinements.

---
+++ LeetCodeSolution.cs
@@ -0,0 +1,29 @@
+// TODO
+// create test class for this
+
+public class LeetCodeSolution {
+    public int MaximumWealth(int[][] accounts) {
+        int maxCustomerWealthSoFar = 0;
+
+        foreach(int[] customerAccountBalances in accounts)
+        {
+            int currentCustomerWealth = 0;
+
+            foreach(int balance in customerAccountBalances)
+            {
+                currentCustomerWealth += balance;
+            }
+
+            // if(customerWealth > maxCustomerWealth)
+            // {
+            //     maxCustomerWealth = customerWealth;
+            // }
+
+            // neater way
+            maxCustomerWealthSoFar = Math.Max(currentCustomerWealth, maxCustomerWealthSoFar);
+
+        }
+
+        return maxCustomerWealthSoFar;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var sol = new LeetCodeSolution();
+int[][] customerAccounts = new int[][] { new int[] {1, 2, 3}, new int[] {6, 3, 2} };
+int maxWealth = sol.MaximumWealth(customerAccounts);
+Console.WriteLine("Maximum customer wealth is: {0}", maxWealth);
