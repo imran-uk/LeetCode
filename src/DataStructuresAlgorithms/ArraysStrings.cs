835f4a7bad086a4252789c1adc0b5408aec4bc34
Imran Chaudhry <ichaudhry@gmail.com>
2022-12-29 10:09:19 +0100

    Added reverseString in-place and sortedSquares

--- src/DataStructuresAlgorithms/ArraysStrings.cs
+++ src/DataStructuresAlgorithms/ArraysStrings.cs
@@ -1,5 +1,4 @@
 
-
 public class ArraysStrings {
 
   public List<char> reverseStringList(List<char> input)
@@ -9,11 +8,47 @@ public class ArraysStrings {
     return input;
   }
 
+  // time/space complexity
+  // time = O(n)
+  // space = O(n) or O(1)
   public void reverseString(char[] input)
   {
-    foreach(var letter in input.Reverse())
+    // modify input in-place
+    // use opposite-direction two-pointers approach somehow
+    // see https://leetcode.com/explore/interview/card/leetcodes-interview-crash-course-data-structures-and-algorithms/703/arraystrings/4501/
+    int left = 0;
+    int right = input.Length - 1;
+
+    // TODO: is this equivalent? it's what i put first in my mind...
+    // would be good tow rite own test cases for this shiz :D
+    // while(left != right)
+    while(left < right)
     {
-      Console.WriteLine($"{letter}");
+      var temp = input[left];
+      input[left] = input[right];
+      input[right] = temp;
+
+      left++;
+      right--;
     }
   }
+
+  // TODO: the exercise says there might be another approach that 
+  // takes O(n)
+  // the main thing i can think of is to sort the input array first 
+  // because the squared number will still be in the same order
+  public int[] SortedSquares(int[] nums) {
+    var squaredList = new List<int>();
+
+    foreach(var element in nums.ToList<int>())
+    {
+      squaredList.Add(element * element);
+    }
+
+    squaredList.Sort();
+
+    return squaredList.ToArray<int>();
+  }
+
+
 }
--- src/DataStructuresAlgorithms/Program.cs
+++ src/DataStructuresAlgorithms/Program.cs
@@ -11,3 +11,24 @@ foreach(var element in reversedString)
 
 char[] s = new char[] { 'h','e','l','l','o' };
 astra.reverseString(s);
+
+// lets see if it worked...
+foreach(var letter in s)
+{
+  // Console.WriteLine($"{letter}");
+}
+
+/*
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+*/
+
+int[] nums = new int[] { -4,-1,0,3,10 };
+var sortedSquares = astra.SortedSquares(nums);
+
+foreach(var element in sortedSquares)
+{
+  Console.WriteLine($"i got me {element}");
+}
